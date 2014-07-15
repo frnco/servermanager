0ade0560dc4ad75a97d099751818caf53dd5b629
Fernando Cordeiro <fernando@fernandocordeiro.com>
2014-07-14 21:34:20 -0300

    Proper parsing of everything.

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ var fs = require('graceful-fs'),
 nginx = [];
 gitHooks = [];
 
+var config = require('./config/server');
+console.log(config.enablegPath);
+
 fs.readdirSync("./config/nginx/").forEach(function(file) {
   if (file != 'example.json') {
     nginx.push(fs.readFileSync("./config/nginx/" + file));
@@ -21,11 +24,23 @@ fs.readdirSync("./config/gitHooks/").forEach(function(file) {
 
 nginx.forEach(function (jsonObj) {
   var servers = JSON.parse(jsonObj);
-  for (var key in servers) {
-    server = servers[key];
+  servers.forEach(function (server) {
     var modelFile = fs.readFileSync('./models/nginx/'+server.type+'.mustache').toString();
 
-    var output = Mustache.render(modelFile, server);
+    var output = Mustache.render(modelFile, {logsFolder: config.logsFolder, server: server});
     console.log(output);
-  }
+  });
+});
+
+gitHooks.forEach(function (jsonObj) {
+  var gitHook = JSON.parse(jsonObj);
+
+  gitHook.hooks.forEach(function (hook) {
+    hook.actions.forEach(function (action) {
+      var modelFile = fs.readFileSync('./models/gitHooks/'+hook.type+'/'+hook.version+'.mustache').toString();
+      
+      var output = Mustache.render(modelFile, {folder: config.logsFolder, action: action});
+    });
+  });
+
 });
